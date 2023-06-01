025dc23a4687d173af064c6501ed8b023f2f9c53
Francesco Rivetti <oha@oha.it>
2023-06-01 07:14:21 +0200

    minor

--- utils/lists/reverse.go
+++ utils/lists/reverse.go
@@ -10,11 +10,26 @@ func Copy[T any](a []T) []T {
 }
 
 func Reverse[T any](a []T) {
-	i := 0
-	j := len(a) - 1
+	i, j := 0, len(a)-1
 	for i < len(a)/2 {
 		a[i], a[j] = a[j], a[i]
 		i++
 		j--
 	}
+}
+
+func Contains[T comparable](a []T, i T) bool {
+	for _, e := range a {
+		if e == i {
+			return true
+		}
+	}
+	return false
+}
+
+func AddUnique[T comparable](a []T, i T) []T {
+	if Contains(a, i) {
+		return a
+	}
+	return append(a, i)
 }
