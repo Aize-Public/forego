1949fbffe26d9d5de183866f542f2bab3decd352
Francesco Rivetti <oha@oha.it>
2023-06-06 16:29:19 +0200

    test: dup old code

--- test/errors.go
+++ test/errors.go
@@ -25,8 +25,6 @@ func NoError(t *testing.T, err error) {
 
 func Error(t *testing.T, err error) {
 	t.Helper()
-	isNil(err).assignment(0, 1).false(t)
-
 	if isNil(err).succeed {
 		Fail(t, "expected error: ", stringy{ast.Assignment(0, 1)})
 	} else {
