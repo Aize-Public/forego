3d7dbee3bca77856062bf7c6c572525824988917
Francesco Rivetti <oha@oha.it>
2023-08-21 08:29:57 +0200

    test: detect terminal

--- test/ctx.go
+++ test/ctx.go
@@ -3,18 +3,26 @@ package test
 import (
 	"context"
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/Aize-Public/forego/ctx"
 	"github.com/Aize-Public/forego/ctx/log"
+	"golang.org/x/sys/unix"
 )
+
+// this ugly thing is true if the output goes to a console, false if the output is piped somewhere
+var isTerminal = func() bool {
+	_, err := unix.IoctlGetTermios(int(os.Stdout.Fd()), unix.TCGETS)
+	return err == nil
+}()
 
 func Context(t *testing.T) ctx.C {
 	t.Helper()
 	c := context.Background()
 	c = ctx.WithTag(c, "test", t.Name())
 	c = log.WithLoggerAndHelper(c, func(ln log.Line) {
-		if testing.Verbose() {
+		if !isTerminal { // TODO(oha) allow for an env variable to override
 			fmt.Println(ln.JSON())
 		} else {
 			t.Helper()
