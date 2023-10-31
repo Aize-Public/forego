855eda18cb631980d202c544e548c3e3e0678ead
Francesco Rivetti <oha@oha.it>
2023-10-31 07:50:54 +0100

    when testing API objects, fully encode/decode

--- api/test.go
+++ api/test.go
@@ -8,9 +8,32 @@ import (
 
 // TODO(oha): should we do marshalling here to be safe?
 func Test[T Op](t *testing.T, op T) T {
+	c := test.Context(t)
 	t.Helper()
-	t.Logf("Test(%+v)...", op)
-	err := op.Do(test.Context(t))
+	h, err := NewHandler(c, op)
+	if err != nil {
+		panic(err)
+	}
+
+	j := &JSON{}
+	err = h.Client().Send(c, op, j)
+	if err != nil {
+		panic(err)
+	}
+	t.Logf("Test[%T](%s)...", op, j.Data)
+	req, err := h.Server().Recv(c, j)
+
+	err = req.Do(test.Context(t))
 	test.NoError(t, err)
+
+	j = &JSON{}
+	err = h.Server().Send(c, req, j)
+	if err != nil {
+		panic(err)
+	}
+	err = h.Client().Recv(c, j, op)
+	if err != nil {
+		panic(err)
+	}
 	return op
 }
